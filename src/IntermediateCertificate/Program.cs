86978c185e2336a29241a9872ffdb70275b4a95d
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2024-02-19 13:41:15 +1300

    Stubbed out certificate generation command line utilties

---
+++ src/IntermediateCertificate/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IntermediateCertificate
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
