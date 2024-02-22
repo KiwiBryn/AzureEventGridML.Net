e20b79a18b20b340a9d5593cf8770102a70cf444
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2024-02-22 13:36:37 +1300

    Stubbed out sstainba version

--- src/YoloV8.sstainbaImage/Program.cs
+++ src/YoloV8.sstainbaImage/Program.cs
@@ -1,4 +1,12 @@
-﻿namespace YoloV8.sstainbaImage
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) February 2023, devMobile Software
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// https://github.com/sstainba/Yolov8.Net
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.AzureEventGrid.YoloV8.sstainba.Image
 {
    internal class Program
    {
