5a09541010152a6edab192bff6736c348f7fb92e
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2024-02-27 18:34:23 +1300

    Renamed Security camera video project to be more consistent, part of ongoing process

---
+++ src/SecurityCamera.Video/Program.cs
@@ -0,0 +1,16 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) February 2023, devMobile Software
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.AzureEventGrid.SecurityCamera.Video
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
