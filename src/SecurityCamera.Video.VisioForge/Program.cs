ac039355d655fb525891cdde8a88286e4540d44d
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2024-02-28 11:58:25 +1300

    Stubbed out visio forge implementation

---
+++ src/SecurityCamera.Video.VisioForge/Program.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="VisioForge">
+//   Video Capture Console Demo.
+// </copyright>
+// <summary>
+//   Defines the Program type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace devMobile.IoT.AzureEventGrid.SecurityCamera.Video
+{
+   static class Program
+   {
+      static void Main(string[] args)
+      {
+      }
+   }
+}
