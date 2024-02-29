a6ae126fe032e4a7e6d47ac392324cf6b1f1ca8d
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2024-02-29 16:47:39 +1300

    First working version of object detection with Niklas Swärd's NuGet

---
+++ src/YoloV8.NickSwardh.Image.Detect/Model.cs
@@ -0,0 +1,17 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) February 2023, devMobile Software
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.AzureEventGrid.YoloV8.NickSwardh.Image.Detect.Model
+{
+   public class ApplicationSettings
+   {
+      public string ImageInputPath { get; set; }
+
+      public string ImageOutputPath { get; set; }
+
+      public string ModelPath { get; set; }
+   }
+}
