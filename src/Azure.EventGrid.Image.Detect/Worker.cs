d0aea030bef4fa834f629e6fd0b3249ed8d13a88
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2024-03-03 14:36:09 +1300

    Removed username and password as won't work with EventGrid

--- src/Azure.EventGrid.Image.Detect/Worker.cs
+++ src/Azure.EventGrid.Image.Detect/Worker.cs
@@ -58,12 +58,7 @@ namespace devMobile.IoT.Azure.EventGrid.Image.Detect
                .WithBroker(_applicationSettings.Host)
                .WithPort(_applicationSettings.Port)
                .WithUserName(_applicationSettings.UserName)
-#if AUTHENTICATION_USERNAME_AND_PASSWORD
-               .WithPassword(_applicationSettings.Password)
-#endif
-#if AUTHENTICATION_CERTIFICATES
                .WithClientCertificate(_applicationSettings.ClientCertificateFileName, _applicationSettings.ClientCertificatePassword)
-#endif
                .WithCleanStart(_applicationSettings.CleanStart)
                .WithUseTls(true);
 
