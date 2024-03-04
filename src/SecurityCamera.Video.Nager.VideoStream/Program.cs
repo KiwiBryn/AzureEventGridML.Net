61ddf5f01d19801905d8f7dbc4d95886a7b10df9
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2024-03-04 16:43:08 +1300

    Stubbed out Nager.VideoStream project

---
+++ src/SecurityCamera.Video.Nager.VideoStream/Program.cs
@@ -0,0 +1,18 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) February 2023, devMobile Software
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// https://github.com/nager/Nager.VideoStream
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.SecurityCamera.Video.Nager.VideoStream
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
