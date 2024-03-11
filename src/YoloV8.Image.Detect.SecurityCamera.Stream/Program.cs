f95a49a12ac4b85185d99f1a2615794e1e9ed186
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2024-03-11 14:58:04 +1300

    Added using and moved to VoloV8 folder

--- src/YoloV8.Image.Detect.SecurityCamera.Stream/Program.cs
+++ src/YoloV8.Image.Detect.SecurityCamera.Stream/Program.cs
@@ -4,12 +4,12 @@
 // http://www.apache.org/licenses/LICENSE-2.0
 //
 //---------------------------------------------------------------------------------
-using System;
-using System.IO;
 using System.Net;
+
+using Microsoft.Extensions.Configuration;
+
 using Compunet.YoloV8;
 using Compunet.YoloV8.Data;
-using Microsoft.Extensions.Configuration;
 
 
 namespace devMobile.IoT.YoloV8.Detect.SecurityCamera.Stream
@@ -37,22 +37,21 @@ namespace devMobile.IoT.YoloV8.Detect.SecurityCamera.Stream
 
             NetworkCredential networkCredential = new NetworkCredential(_applicationSettings.CameraUserName, _applicationSettings.CameraUserPassword);
 
-            _httpClient = new HttpClient(new HttpClientHandler { PreAuthenticate = true, Credentials = networkCredential });
-
-            _predictor = YoloV8Predictor.Create(_applicationSettings.ModelPath);
-
-            Timer imageUpdatetimer = new Timer(ImageUpdateTimerCallback, null, _applicationSettings.ImageTimerDue, _applicationSettings.ImageTimerPeriod);
-
-            Console.WriteLine($" {DateTime.UtcNow:yy-MM-dd HH:mm:ss} press <ctrl^c> to exit");
-
-            try
+            using (_httpClient = new HttpClient(new HttpClientHandler { PreAuthenticate = true, Credentials = networkCredential }))
+            using (_predictor = YoloV8Predictor.Create(_applicationSettings.ModelPath))
             {
-               await Task.Delay(Timeout.Infinite);
-            }
-            catch (TaskCanceledException)
-            {
-               Console.WriteLine($"{DateTime.UtcNow:yy-MM-dd HH:mm:ss} Application shutown requested");
+              Timer imageUpdatetimer = new Timer(ImageUpdateTimerCallback, null, _applicationSettings.ImageTimerDue, _applicationSettings.ImageTimerPeriod);
 
+               Console.WriteLine($" {DateTime.UtcNow:yy-MM-dd HH:mm:ss} press <ctrl^c> to exit");
+
+               try
+               {
+                  await Task.Delay(Timeout.Infinite);
+               }
+               catch (TaskCanceledException)
+               {
+                  Console.WriteLine($"{DateTime.UtcNow:yy-MM-dd HH:mm:ss} Application shutown requested");
+               }
             }
          }
          catch (Exception ex)
@@ -72,7 +71,7 @@ namespace devMobile.IoT.YoloV8.Detect.SecurityCamera.Stream
 
          try
          {
-            Console.WriteLine($" {DateTime.UtcNow:yy-MM-dd HH:mm:ss:fff} YoloV8 Security Camera Image processing start");
+            Console.WriteLine($"{DateTime.UtcNow:yy-MM-dd HH:mm:ss:fff} YoloV8 Security Camera Image processing start");
 
             DetectionResult result;
 
@@ -81,17 +80,18 @@ namespace devMobile.IoT.YoloV8.Detect.SecurityCamera.Stream
                result = await _predictor.DetectAsync(cameraStream);
             }
 
-            Console.WriteLine($" Speed: {result.Speed}");
+            Console.WriteLine($"Speed: {result.Speed}");
 
             foreach (var prediction in result.Boxes)
             {
-               Console.WriteLine($"  Class {prediction.Class} {(prediction.Confidence * 100.0):f1}% X:{prediction.Bounds.X} Y:{prediction.Bounds.Y} Width:{prediction.Bounds.Width} Height:{prediction.Bounds.Height}");
+               Console.WriteLine($" Class {prediction.Class} {(prediction.Confidence * 100.0):f1}% X:{prediction.Bounds.X} Y:{prediction.Bounds.Y} Width:{prediction.Bounds.Width} Height:{prediction.Bounds.Height}");
             }
-            Console.WriteLine($" {DateTime.UtcNow:yy-MM-dd HH:mm:ss:fff} YoloV8 Security Camera Image processing done");
+
+            Console.WriteLine($"{DateTime.UtcNow:yy-MM-dd HH:mm:ss:fff} YoloV8 Security Camera Image processing done");
          }
          catch (Exception ex)
          {
-            Console.WriteLine($"{DateTime.UtcNow:yy-MM-dd HH:mm:ss} Security camera image download failed {ex.Message}");
+            Console.WriteLine($"{DateTime.UtcNow:yy-MM-dd HH:mm:ss} YoloV8 Security camera image download or YoloV8 prediction failed {ex.Message}");
          }
          finally
          {
