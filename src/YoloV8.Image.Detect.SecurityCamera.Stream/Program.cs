c017fa29b03dc0410f05bab5717ae9f25aa5d5eb
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2024-03-11 14:01:13 +1300

    Stubbed out project so sln merge less painful

---
+++ src/YoloV8.Image.Detect.SecurityCamera.Stream/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace YoloV8.Image.Detect.SecurityCamera.Stream
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
