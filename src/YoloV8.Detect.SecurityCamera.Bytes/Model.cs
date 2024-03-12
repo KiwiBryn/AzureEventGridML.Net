f2923d082d5e4d7891173abe8079389e16a6d10f
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2024-03-12 19:16:03 +1300

    Added sample with image as an array of bytes

---
+++ src/YoloV8.Detect.SecurityCamera.Bytes/Model.cs
@@ -0,0 +1,20 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) February 2023, devMobile Software
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.YoloV8.Detect.SecurityCamera.Image.Bytes.Model
+{
+	public class ApplicationSettings
+   {
+      public TimeSpan ImageTimerDue { get; set; }
+      public TimeSpan ImageTimerPeriod { get; set; }
+
+      public string CameraUrl { get; set; }
+      public string CameraUserName { get; set; }
+      public string CameraUserPassword { get; set; }
+
+      public string ModelPath { get; set; }
+   }
+}
---
+++ src/YoloV8.Detect.SecurityCamera.Bytes/Program.cs
@@ -0,0 +1,99 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) March 2023, devMobile Software
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+//---------------------------------------------------------------------------------
+using System.Net;
+
+using Microsoft.Extensions.Configuration;
+
+using Compunet.YoloV8;
+using Compunet.YoloV8.Data;
+
+
+namespace devMobile.IoT.YoloV8.Detect.SecurityCamera.Image.Bytes
+{
+   class Program
+   {
+      private static Model.ApplicationSettings _applicationSettings;
+      private static bool _cameraBusy = false;
+      private static HttpClient _httpClient;
+      private static YoloV8Predictor _predictor;
+
+      static async Task Main(string[] args)
+      {
+         Console.WriteLine($"{DateTime.UtcNow:yy-MM-dd HH:mm:ss} SecurityCameraImageStream starting");
+
+         try
+         {
+            // load the app settings into configuration
+            var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", false, true)
+                 .AddUserSecrets<Program>()
+            .Build();
+
+            _applicationSettings = configuration.GetSection("ApplicationSettings").Get<Model.ApplicationSettings>();
+
+            NetworkCredential networkCredential = new NetworkCredential(_applicationSettings.CameraUserName, _applicationSettings.CameraUserPassword);
+
+            using (_httpClient = new HttpClient(new HttpClientHandler { PreAuthenticate = true, Credentials = networkCredential }))
+            using (_predictor = YoloV8Predictor.Create(_applicationSettings.ModelPath))
+            {
+               Timer imageUpdatetimer = new Timer(ImageUpdateTimerCallback, null, _applicationSettings.ImageTimerDue, _applicationSettings.ImageTimerPeriod);
+
+               Console.WriteLine($" {DateTime.UtcNow:yy-MM-dd HH:mm:ss} press <ctrl^c> to exit");
+
+               try
+               {
+                  await Task.Delay(Timeout.Infinite);
+               }
+               catch (TaskCanceledException)
+               {
+                  Console.WriteLine($"{DateTime.UtcNow:yy-MM-dd HH:mm:ss} Application shutown requested");
+               }
+            }
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine($"{DateTime.UtcNow:yy-MM-dd HH:mm:ss} Application shutown failure {ex.Message}", ex);
+         }
+      }
+
+      private static async void ImageUpdateTimerCallback(object state)
+      {
+         // Just incase - stop code being called while photo already in progress
+         if (_cameraBusy)
+         {
+            return;
+         }
+         _cameraBusy = true;
+
+         try
+         {
+            Console.WriteLine($"{DateTime.UtcNow:yy-MM-dd HH:mm:ss:fff} YoloV8 Security Camera Image processing start");
+
+            byte[] bytes = await _httpClient.GetByteArrayAsync(_applicationSettings.CameraUrl);
+
+            DetectionResult result = await _predictor.DetectAsync(bytes);
+
+            Console.WriteLine($"Speed: {result.Speed}");
+
+            foreach (var prediction in result.Boxes)
+            {
+               Console.WriteLine($" Class {prediction.Class} {(prediction.Confidence * 100.0):f1}% X:{prediction.Bounds.X} Y:{prediction.Bounds.Y} Width:{prediction.Bounds.Width} Height:{prediction.Bounds.Height}");
+            }
+
+            Console.WriteLine($"{DateTime.UtcNow:yy-MM-dd HH:mm:ss:fff} YoloV8 Security Camera Image processing done");
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine($"{DateTime.UtcNow:yy-MM-dd HH:mm:ss} YoloV8 Security camera image download or YoloV8 prediction failed {ex.Message}");
+         }
+         finally
+         {
+            _cameraBusy = false;
+         }
+      }
+   }
+}
