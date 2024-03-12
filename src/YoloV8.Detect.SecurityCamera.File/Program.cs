b9a0bd2ddffee5b7c3993628cdb31e8ae2f278e7
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2024-03-12 14:49:21 +1300

    Stubbed out file download version of YoloV8 security camera integration

---
+++ src/YoloV8.Detect.SecurityCamera.File/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace YoloV8.Detect.SecurityCamera.File
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
