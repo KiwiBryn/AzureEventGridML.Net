b3d7435f58efb0625c15fcd4c317699f25b240b6
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2024-03-27 11:43:41 +1300

    Subbed out Pose & Detect projects before split

---
+++ src/Azure.EventGrid.Image.Detect/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Azure.EventGrid.Image.Detect
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
---
+++ src/Azure.EventGrid.Image.Pose/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Azure.EventGrid.Image.Pose
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
