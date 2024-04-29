2b7b32815a0233a8860ee83958a446faad466ec9
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2024-04-29 19:08:53 +1200

    Tidy up of Azure ML trained model code. WOn't bother with namespace

---
+++ src/FasterrCNNResnet50.Detect.Image/Model.cs
@@ -0,0 +1,20 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) April 2024 devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+//
+// https://github.com/dotnet/MQTTnet
+//---------------------------------------------------------------------------------
+namespace devMobile.FasterrCNNResnet50.Detect.Image.Model
+{
+   internal class ApplicationSettings
+   {
+      public string ImageInputPath { get; set; }
+
+      public string ImageOutputPath { get; set; }
+
+      public string ModelPath { get; set; }
+   }
+}
+
+
